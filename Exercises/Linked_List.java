3bffb5648b1cb3e7df5a356337b3a8e35f3051c0
Mary Jasmin <maryjasminpancho@gmail.com>
2025-04-15 00:24:48 +0800

    Add existing file

---
+++ Exercises/Linked_List.java
@@ -0,0 +1,38 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Linked_List 
+{
+   public static void main(String[] args)
+   {
+      Scanner sc = new Scanner(System.in);
+      LinkedList<>
+      
+      do 
+      {
+         int choice;
+         
+         System.out.println("[0] Terminate the Program");
+         System.out.println("[1] Insert an Element");
+         System.out.println("[2] Delete an Element");
+         System.out.println("[3] Display all Elements");
+         System.out.println("[4] Search an Element");
+         System.out.println("[5] Delete an Element");
+         System.out.print("\nEnter your choice: ");
+         choice = sc.nextInt();
+         
+         switch(choice) 
+         {
+            case 0:
+               System.out.println("Exiting the program...");
+               break;
+               
+            case 1:
+               
+               
+         }
+         
+      } while (choice != 0);
+   }
+}
+
